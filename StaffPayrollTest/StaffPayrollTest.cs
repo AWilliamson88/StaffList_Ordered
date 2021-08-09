38b0f9eb17670e619d840332c87ef4202071bbad
AWilliamson88 <87967861+AWilliamson88@users.noreply.github.com>
2021-08-09 11:06:26 +0800

    Playing with Unit Testing
    No updates to the code.
    Just playing around seeing how the unit testing worked.

--- StaffPayrollTest/StaffPayrollTest.cs
+++ StaffPayrollTest/StaffPayrollTest.cs
@@ -9,11 +9,65 @@ namespace StaffPayrollTest
     public class StaffPayrollTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NamesAdded_AppearInTree()
+        {
+            Tree t = new Tree();
+            
+            t.Add("Ava");
+            t.Add("Zoe");
+
+            Assert.IsTrue(t.Find("Ava"));
+        }
+
+        [TestMethod]
+        public void Searching_NameInList_IsFound()
         {
             Tree t = new Tree();
 
-            
+            t.Add("Ava");
+            t.Add("Zoe");
+
+            Assert.IsTrue(t.Find("Ava"));
         }
+
+        [TestMethod]
+        public void Searching_NameNotInList_IsNotFound()
+        {
+            Tree t = new Tree();
+
+            t.Add("Ava");
+            t.Add("Zoe");
+
+            Assert.IsFalse(t.Find("Paul"));
+        }
+
+        [TestMethod]
+        public void Delete_NameNotInList_DoesNotDeleteAnything()
+        {
+            Tree t = new Tree();
+
+            t.Add("Ava");
+            t.Add("Zoe");
+
+            t.Delete("A");
+
+            Assert.IsTrue(t.Find("Ava") && t.Find("Zoe"));
+        }
+
+        [TestMethod]
+        public void Delete_NameInList_DeletesThatName()
+        {
+            Tree t = new Tree();
+
+            t.Add("Ava");
+            t.Add("Zoe");
+
+            t.Delete("Zoe");
+
+            Assert.IsTrue(t.Find("Ava") && !t.Find("Zoe"));
+        }
+
+
+
     }
 }
